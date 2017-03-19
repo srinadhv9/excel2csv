c30d09b0110a73d89bee3dc1b5ec41cc3d8350da
Srinadh <srinadh.ventrapragada@ge.com>
2017-03-19 12:47:08 -0400

    Changes

--- src/excel2csv/ExecutePS.java
+++ src/excel2csv/ExecutePS.java
@@ -9,7 +9,10 @@ public class ExecutePS {
 		try {
 			PowerShell powerShell;
 			powerShell = PowerShell.openSession();
-			powerShell.executeCommand("powershell -ExecutionPolicy ByPass -File C:\\Users\\502362723\\Desktop\\xlsbtoxlsx.ps1 'C:\\Users\\502362723\\Desktop\\10-Mar-2017-CPS-Report-FW-10_LV' Set-ExecutionPolicy RemoteSigned");
+			String scriptFilePath = "C:\\Users\\502362723\\Desktop\\xlsbtoxlsx.ps1";
+			String sourceFile = "C:\\Users\\502362723\\Desktop\\10-Mar-2017-CPS-Report-FW-10_LV";
+			String command = "powershell -ExecutionPolicy ByPass -File " + scriptFilePath + "  '" + sourceFile + "' Set-ExecutionPolicy RemoteSigned";
+			powerShell.executeCommand(command);
 			powerShell.close();
 		} catch (PowerShellNotAvailableException e) {
 			e.printStackTrace();
